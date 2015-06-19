9a07175a6497fa5266d7084bb61241904094b539
asus <asus@asus-X450CC>
2015-06-19 17:48:31 -0500

    #2 - comap

---
+++ js/jcarousel.skeleton.js
@@ -0,0 +1,92 @@
+(function($) {
+    jQuery(function() {
+    jQuery('.jcarousel')
+            .jcarousel({
+                // Options go here
+            });
+
+        
+        jQuery('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                jQuery(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                jQuery(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '-=1'
+            });
+
+        
+        jQuery('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                jQuery(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                jQuery(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '+=1'
+            });
+
+        jQuery('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                jQuery(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                jQuery(this).removeClass('active');
+            })
+            .jcarouselPagination({
+                // Options go here
+            });
+    
+});
+})(jQuery);
+/*(function($) {
+    jQuery(function() {
+        
+        jQuery('.jcarousel')
+            .jcarousel({
+                // Options go here
+            });
+
+        
+        jQuery('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                jQuery(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                jQuery(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '-=1'
+            });
+
+        
+        jQuery('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                jQuery(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                jQuery(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '+=1'
+            });
+
+        jQuery('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                jQuery(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                jQuery(this).removeClass('active');
+            })
+            .jcarouselPagination({
+                // Options go here
+            });
+    });
+})(jQuery);*/
